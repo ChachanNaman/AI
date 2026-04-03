b8f3109689f5ed24e5e8a3bf75386433def53c07
Naman Chachan <143930974+ChachanNaman@users.noreply.github.com>
2026-04-04 00:57:49 +0530

    chatbot(prompting)

--- Langchain_Prompts/3Chatbot.py
+++ Langchain_Prompts/3Chatbot.py
@@ -7,5 +7,5 @@ model = ChatGroq(
     model="llama-3.1-8b-instant"
 )
 
-#as its chatbot , it will run infinitely until user ends itself
+#as its chatbot , it will run infinitely until user ends itself. 
 
